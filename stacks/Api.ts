d622b47393dfdc2df63a108a859df0f20220f6a1
Ty Rauber <tyrauber@mac.com>
2023-04-19 15:34:50 -0400

    fix: remove the authorization on API Gateway. No longer needed, we are authorizing at the cloudfront distribution with JWTs.

--- stacks/Api.ts
+++ stacks/Api.ts
@@ -4,18 +4,8 @@ import { use, StackContext, Function, Api as ApiGateway } from "sst/constructs";
 export function Api({ stack }: StackContext) {
     const bucket = use(Bucket);
     const api = new ApiGateway(stack, "Api", {
-        authorizers: {
-          lambda: {
-            type: "lambda",
-            responseTypes: ["simple"],
-            function: new Function(stack, "Authorizer", {
-                handler: "packages/functions/src/authorizer.main",
-            }),
-          },
-        },
         defaults: {
             function: {
-                authorizer: "lambda",
                 environment: { 
                     BUCKET: bucket.bucketName, 
                     PMTILES_PATH: "tiles/{name}.pmtiles"
@@ -25,16 +15,13 @@ export function Api({ stack }: StackContext) {
         },
         routes: {
             "GET /v1/private": {
-                function: "packages/functions/src/private.main",
-                authorizer: "lambda",
+                function: "packages/functions/src/private.main"
             },
             "GET /v1/public": {
-                function: "packages/functions/src/public.main",
-                authorizer: "none",
+                function: "packages/functions/src/public.main"
             },
             "GET /v1/tiles/{proxy+}":{
-                function: "packages/functions/src/tiles.main",
-                authorizer: "none",
+                function: "packages/functions/src/tiles.main"
             },
             // $default: {
             //     function: "packages/functions/src/tiles.main",
